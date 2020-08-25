f0124120db69b7e8fe520b34e0d734d6c79b90a0
Adam Grossman <adgrossm@gmail.com>
2020-08-25 09:40:45 -0400

    created question array, have answers console.log, getting error when trying to push to empty readMe string

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+// const util = require("util");
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+// function promptUser(){
+
+// return
 inquirer.prompt([
     // this is where we will put the prompts for the questions we need to ask to build out the array
     {
         type: "input",
         message: "What is your GitHub username?",
-        name: "gitHub"
+        name: "username"
         },
         {
         type: "input",
@@ -16,12 +22,12 @@ inquirer.prompt([
         {
         type: "input",
         message: "What is the name of your project?",
-        name: "projectName"
+        name: "title"
         },
         {
         type: "input",
         message: "Please write a short description of your project:",
-        name: "projectDescrip"
+        name: "description"
         },
         {
         type: "list",
@@ -33,35 +39,39 @@ inquirer.prompt([
         type: "list",
         message: "What command should be run to install dependencies?",
         choices: ["npm i"],
-        name: "commandInstall"
+        name: "installation"
         },
             {
                 type: "list",
                 message: "What command should be run to run tests?",
                 choices: ["npm test"],
-                name: "commandTests"
+                name: "test"
             },
             {
             type: "input",
             message: "What does the user need to know about using the repo?",
-            name: "repoUsage"
+            name: "usage"
             },
             {
             type: "input",
             message: "What does the user need to know about contributing to the repo?",
-            name: "repoContrib"
+            name: "contributing"
             },
-    
-]).then(answers => {
+        ])
+      
+.then(answers => {
     console.log(answers);
 
-    let readme = "";
+    let readMe = "";
 
-    fs.writeFile("README.md", readme,  err => {
+    fs.writeFile("README.md", readMe,  err => {
         if (err){
             return console.log(err);
-
+    readMe.push(answers.title);
+    readMe.push(data.description);
         }
+        
     });
+    
 });
-// step 1 figure out how markdown works. 
+
